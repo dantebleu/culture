1fece695117197a994412bc78d2b050d78d5aa8c
dantebleu <31429401+dantebleu@users.noreply.github.com>
2023-05-30 22:18:48 -0500

    added tailwind css

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,7 @@
 function App() {
-  return (
-    <div>
-      <h1>content here</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
+
+// https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com for inspiration
