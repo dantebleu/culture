a039b9f266afc5be2468916505c628415bd13875
dantebleu <31429401+dantebleu@users.noreply.github.com>
2023-06-05 15:45:51 -0500

    changes to button and layout

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,62 +1,71 @@
 export default function App() {
   return (
-    <div className="flex justify-center">
-      <div className="w-480">
-        <h1>log in</h1>
-        <form action="">
-          {/* email label section */}
-          <div>
-            <label For="email">Email</label>
-            <span>
-              Need an account? <span>Sign Up</span>{" "}
-            </span>
-            <div>
+    <div className="flex justify-center items-center h-screen bg-indigo-600">
+      <div className="w-96 p-6 bg-white rounded-md shadow-lg">
+        <h1 className="text-3xl font-bold">Log in</h1>
+        <div className="">
+          <form action="" className="">
+            {/* email label section */}
+            <div
+              className="mt-7
+            "
+            >
+              <label For="email" className="font-medium text-base mb-2">
+                Email
+              </label>
               <input
-                className="border border-gray-300 rounded-md px-4 py-2"
+                className="w-full border border-gray-300 rounded-md p-2 mt-2"
                 type="text"
                 name=""
                 id="email"
               />
             </div>
-          </div>
-          {/* password section */}
-          <div>
-            <div>
-              <label htmlFor="">PassWord</label>
-              <span>Show</span>
+            {/* password section */}
+            <div className="mt-6">
+              <div className="justify-between flex">
+                <label For="password" className="font-medium text-base mb-2 ">
+                  Password
+                </label>
+                <span>Show</span>
+              </div>
+              <div>
+                <input
+                  className="w-full border border-gray-300 rounded-md p-2 mt-2"
+                  type="password"
+                  id="password"
+                />
+              </div>
             </div>
-            <div>
-              <input
-                className="border border-gray-300 rounded-md px-4 py-2"
-                type="text"
-              />
-            </div>
-          </div>
-        </form>
-        {/* button sections */}
+          </form>
+        </div>
 
-        <div>
-          <div className="flex justify-center">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-3/4">
+        {/* button sections */}
+        <div className="mt-5">
+          <div className="">
+            <button className="w-full rounded-full bg-blue-500 hover:bg-blue-700 text-white font-bold p-3">
               Log in
             </button>
           </div>
-          <hr />
-          <div className="flex justify-center">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-3/4">
-              facebook
+          <div className="pt-6 flex justify-between items-center">
+            <hr className="border-gray-400 w-full" />
+            <p className="px-2 font-light tracking-wider text-gray-500">OR</p>
+            <hr className="border-gray-400 w-full" />
+          </div>
+          <div className="mt-3">
+            <button className="rounded-full bg-blue-500 hover:bg-blue-700 text-white font-bold">
+              Facebook
             </button>
           </div>
           <hr />
-          <div className="flex justify-center">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-3/4">
-              google
+          <div className="">
+            <button className="rounded-full bg-blue-500 hover:bg-blue-700 text-white font-bold">
+              Google
             </button>
           </div>
           <hr />
-          <div className="flex justify-center">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-3/4">
-              github
+          <div className="">
+            <button className="rounded-full bg-blue-500 hover:bg-blue-700 text-white font-bold">
+              GitHub
             </button>
           </div>
         </div>
