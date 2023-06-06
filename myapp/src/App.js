cf38355be8d086281c11fa45d7b32b62331daccf
dantebleu <31429401+dantebleu@users.noreply.github.com>
2023-06-06 00:37:37 -0500

    added styling to buttons and login page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-indigo-600">
-      <div className="w-96 p-6 bg-white rounded-md shadow-lg">
+      <div className="w-[480px] p-[48px] bg-white rounded-md shadow-lg ">
         <h1 className="text-3xl font-bold">Log in</h1>
         <div className="">
           <form action="" className="">
@@ -42,31 +42,34 @@ export default function App() {
         {/* button sections */}
         <div className="mt-5">
           <div className="">
-            <button className="w-full rounded-full bg-blue-500 hover:bg-blue-700 text-white font-bold p-3">
+            <button className="w-full rounded-full bg-indigo-600 hover:bg-indigo-400 text-white font-bold p-3">
               Log in
             </button>
           </div>
           <div className="pt-6 flex justify-between items-center">
             <hr className="border-gray-400 w-full" />
-            <p className="px-2 font-light tracking-wider text-gray-500">OR</p>
+            <p className="px-2 font-light tracking-wider text-black text-sm">
+              OR
+            </p>
             <hr className="border-gray-400 w-full" />
           </div>
-          <div className="mt-3">
-            <button className="rounded-full bg-blue-500 hover:bg-blue-700 text-white font-bold">
-              Facebook
+          <div className="mt-5">
+            <button className="w-full rounded-full bg-blue-500 hover:bg-blue-700 text-white text-[16px] font-normal p-3">
+              Continue with Facebook
             </button>
           </div>
-          <hr />
-          <div className="">
-            <button className="rounded-full bg-blue-500 hover:bg-blue-700 text-white font-bold">
-              Google
+          <div className="mt-2">
+            <button className="w-full border border-gray-300 rounded-full bg-white hover:bg-gray-300 text-black text-[16px] font-normal p-3">
+              Continue with Google
             </button>
           </div>
-          <hr />
-          <div className="">
-            <button className="rounded-full bg-blue-500 hover:bg-blue-700 text-white font-bold">
-              GitHub
+          <div className="mt-2">
+            <button className="w-full border border-gray-300 rounded-full bg-white hover:bg-blue-700 text-black text-[16px] font-normal p-3">
+              Continue with GitHub
             </button>
+          </div>
+          <div className="mt-5 flex justify-center items-center text-[13px] text-[#282a35] font-semibold">
+            <a href="www.google.com">Forgot Password?</a>
           </div>
         </div>
       </div>
